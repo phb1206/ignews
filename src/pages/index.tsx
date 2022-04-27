e5964438f9e94f49e62fb5560156a97225dc2bf8
Pedro Brandão <phbrandao1206@gmial.com>
2022-04-27 18:14:06 +0200

    added header and signin components, advanced on home page only subscribe button is missing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src='/images/avatar.svg' alt='Coding sister' />
+      </main>
     </>
   )
 }
